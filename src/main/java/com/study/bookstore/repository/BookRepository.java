265fd37d3ef032d81fc42d53be10aca39dd7dbc7
TassioTristao <tassio.tristao05@gmail.com>
2025-05-26 08:57:50 -0300

    feat: add BookRepository and AuthorRepository interfaces

--- src/main/java/com/study/bookstore/repository/BookRepository.java
+++ src/main/java/com/study/bookstore/repository/BookRepository.java
@@ -1,8 +1,7 @@
 package com.study.bookstore.repository;
 
+import com.study.bookstore.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.awt.print.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
